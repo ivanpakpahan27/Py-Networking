c757244da0eff5f4f72c1ffdd1b0dd7d4f70e02a
ivanpakpahan27 <ivanpakpahangn@gmail.com>
2023-05-25 12:30:50 +0700

    -

--- network.py
+++ network.py
@@ -1,7 +1,10 @@
+import inspect
 import requests
 import webbrowser
 import subprocess
 import ipaddress
+import os
+import socket
 
 # Modul Requests
 # req = requests.get('http://google.com')
@@ -14,15 +17,15 @@ import ipaddress
 # webbrowser.open(address)
 
 # Modul Subprocess
-for ping in range(72, 75):
-    address = "192.168.2." + str(ping)
-    res = subprocess.call('ping /n 2 /w 1000 %s' % address)
-    if res == 0:
-        print("Ping ke alamat "+address+" OK")
-    elif res == 1:
-        print("Tiada respon dari "+address)
-    else:
-        print("Ping ke alamat "+address+" Gagal")
+# for ping in range(72, 75):
+#     address = "192.168.2." + str(ping)
+#     res = subprocess.call('ping /n 2 /w 1000 %s' % address)
+#     if res == 0:
+#         print("Ping ke alamat "+address+" OK")
+#     elif res == 1:
+#         print("Tiada respon dari "+address)
+#     else:
+#         print("Ping ke alamat "+address+" Gagal")
 
 # Modul IP Address
 # myIpAddress = input('Enter an IP Address with a CIDR >>> ')  # 192.168.0.1/24
@@ -35,3 +38,25 @@ for ping in range(72, 75):
 #         print('I can ping %s.' % str(i))
 #     if canPing == 1:
 #         print('%s is not responding' % str(i))
+
+# Modul os.popen
+# ip = input("IP Address Target : ")
+# results = os.popen("pathping " + str(ip))
+# for i in results:
+#     print(i)
+# print(inspect.getargspec(os.popen))
+
+# Modul Socket
+# Create a new socket using the given address family
+socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Setting up the default timeout in seconds for new socket object
+socket.setdefaulttimeout(1)
+addr = "192.168.2.75"  # Ip Address target dapat diketahui dari hasil ping
+port = 80
+result = socket_obj.connect_ex((addr, port))
+if result == 0:
+    print("Connected")
+else:
+    print("Disconnect")
+socket_obj.close()
